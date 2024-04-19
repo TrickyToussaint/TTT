5914e9668dc06f9055d11da5e9668ad1ec1772e7
TrickyToussaint <Gme12Lme90@proton.me>
2024-04-19 07:08:30 -0400

    First Commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public bool GameOver = true;
+    public bool Player1Turn = false;
+    public bool Player2Turn = false;
+    public List<GameObject> BluePlayingPieces;
+    public List<GameObject> RedPlayingPieces;
+    public List<GameObject> PieceSpots; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
